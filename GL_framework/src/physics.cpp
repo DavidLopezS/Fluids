5001e4227065454bfc7389aad4b37520f146c7fc
dlopez <dlopez@G1-49>
2017-05-16 14:03:50 +0200

    Its something

--- GL_framework/src/physics.cpp
+++ GL_framework/src/physics.cpp
@@ -1,7 +1,99 @@
-#include <imgui\imgui.h>
+﻿#include <imgui\imgui.h>
 #include <imgui\imgui_impl_glfw_gl3.h>
+#include <GL\glew.h>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <cstdio>
+#include <iostream>
+#include <math.h>
+#include <time.h>
 
 bool show_test_window = false;
+
+float sphereRadius = rand() % 3 + 0.5f;
+glm::vec3 spherePos(rand() % 7 - 3, rand() % 7 + 1 - sphereRadius, rand() % 7 - 3);
+
+namespace Sphere {
+	extern void updateSphere(glm::vec3 pos = spherePos, float radius = sphereRadius);
+}
+
+namespace ClothMesh {
+	extern void updateClothMesh(float *array_data);
+}
+
+glm::vec3 gravity = { 0, -9.8f, 0 };
+
+int col = 14, row = 18;
+int clothLength = col*row; // 14*18=252
+
+class Particle {
+public:
+	glm::vec3 pos; //x
+	float yPos;
+	glm::vec3 direction;//Maybe in another class
+};
+Particle* fluidSurface = new Particle[clothLength];
+float* vertArray = new float[clothLength * 3];
+
+class Wave{
+public:
+	float A;//Amplitud
+	float w;//Omega
+	glm::vec3 k;//Wave vector
+	glm::vec3 Φ;//Phi, desplaça el punt d'inici de la wave
+};
+int numberOfWaves = 1;
+Wave w1;
+
+float springColumn = 0.76f;
+float springRow = 0.58f;
+float fluidHeight = 3;
+
+void initializeCloth() {
+	fluidSurface[0].pos = { -(13 * springColumn / 2),fluidHeight,-(17 * springRow / 2) };
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < col; ++j) {
+			fluidSurface[i*col + j].pos = { fluidSurface[0].pos.x + j*springColumn ,fluidSurface[0].pos.y ,fluidSurface[0].pos.z + i*springRow };
+		}
+	}
+}
+
+void particleToFloatConverter() {
+	for (int i = 0; i < 252; ++i) {
+		vertArray[i * 3 + 0] = fluidSurface[i].pos.x;
+		vertArray[i * 3 + 1] = fluidSurface[i].yPos;
+		vertArray[i * 3 + 2] = fluidSurface[i].pos.z;
+	}
+}
+
+void updateWavePos(float time) {
+	glm::vec3 prePos;
+	for(int i = 0; i < 252; ++i){
+		prePos = fluidSurface[i].pos;
+		fluidSurface[i].pos = prePos - (w1.k / glm::length(w1.k))  * w1.A * glm::sin(glm::dot(w1.k, prePos) - (w1.w * time));
+		fluidSurface[i].pos.y = 0;
+		fluidSurface[i].yPos = w1.A * glm::cos(glm::dot(w1.k, prePos) - (w1.w * time));
+	}
+	
+}
+
+void PhysicsInit() {
+	initializeCloth();
+	w1.A = 1.f;
+	w1.k = {0.3, 0, 0};
+	w1.w = 10.f;
+}
+void PhysicsUpdate(float dt) {
+	//TODO
+	updateWavePos(dt);
+	particleToFloatConverter();
+	ClothMesh::updateClothMesh(vertArray);
+}
+void PhysicsCleanup() {
+	delete[] fluidSurface;
+	delete[] vertArray;
+}
+
 void GUI() {
 	{	//FrameRate
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -10,18 +102,8 @@ void GUI() {
 	}
 
 	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-	if(show_test_window) {
+	if (show_test_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::ShowTestWindow(&show_test_window);
 	}
-}
-
-void PhysicsInit() {
-	//TODO
-}
-void PhysicsUpdate(float dt) {
-	//TODO
-}
-void PhysicsCleanup() {
-	//TODO
 }
